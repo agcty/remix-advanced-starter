044b2a76627bdb24b086b95e76f5e10304fc5f8a
Alex <alex@gogl.io>
2024-07-30 17:04:55 +0200

    add todos

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -2,5 +2,6 @@ import Database from "better-sqlite3"
 import { drizzle } from "drizzle-orm/better-sqlite3"
 import * as schema from "../../schema/users"
 
+// TODO: Replace with your own database connection for production
 const sqlite = new Database("sqlite.db")
 export const db = drizzle(sqlite, { schema })
