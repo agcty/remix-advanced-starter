efea8d164e56744b68e336a771468a2751cd6615
Alex <alex@gogl.io>
2024-08-01 16:29:46 +0200

    move db

--- db.server.ts
+++ db.server.ts
@@ -3,5 +3,5 @@ import { drizzle } from "drizzle-orm/better-sqlite3"
 import * as schema from "./schema/multitenancy"
 
 // TODO: Replace with your own database connection for production
-const sqlite = new Database("sqlite.db")
-export const db = drizzle(sqlite, { schema })
+export const connection = new Database("sqlite.db")
+export const db = drizzle(connection, { schema })
