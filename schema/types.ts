62a9402f36606af3e14ea68d3c0302f5a31bc2f5
Alex <alex@gogl.io>
2024-08-03 12:55:59 +0200

    improve tx type

--- schema/types.ts
+++ schema/types.ts
@@ -1,4 +1,5 @@
 import type Database from "better-sqlite3"
+import { type db } from "db.server"
 import { type ExtractTablesWithRelations } from "drizzle-orm"
 import {
   type BaseSQLiteDatabase,
@@ -6,18 +7,8 @@ import {
 } from "drizzle-orm/sqlite-core"
 import type * as schema from "schema"
 
+type Database = typeof db | Parameters<Parameters<typeof db.transaction>[0]>[0]
+
 export type TransactionParam = {
-  tx?:
-    | SQLiteTransaction<
-        "sync",
-        Database.RunResult,
-        typeof schema,
-        ExtractTablesWithRelations<typeof schema>
-      >
-    | BaseSQLiteDatabase<
-        "sync",
-        Database.RunResult,
-        typeof schema,
-        ExtractTablesWithRelations<typeof schema>
-      >
+  tx?: Database
 }
