3f50593e04afab399b565ccb7843f6e873a1fc63
Alex <alex@gogl.io>
2024-08-04 01:22:28 +0200

    remove unneeded

--- db.server.ts
+++ db.server.ts
@@ -1,6 +1,5 @@
 import Database from "better-sqlite3"
 import { drizzle } from "drizzle-orm/better-sqlite3"
-// import { drizzle } from "drizzle-orm/d1"
 import * as schema from "schema"
 
 export const connection = new Database(process.env.DATABASE_URL)
