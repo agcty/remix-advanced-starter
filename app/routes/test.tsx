d99c95ca55dbb0902e3efecdcb983e683eb72e84
Alex <alex@gogl.io>
2024-08-17 14:03:46 +0200

    add stuff

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,13 +1,19 @@
-import { type LoaderFunctionArgs } from "@remix-run/node"
+import { json, type LoaderFunctionArgs } from "@remix-run/node"
 import { db } from "db.server"
 import { Switch, SwitchField, SwitchGroup } from "~/components/ui/Switch"
 import { Breadcrumbs } from "~/components/ui2/Breadcrumbs"
 import { Button } from "~/components/ui2/Button"
 import { ComboBox, ComboBoxItem } from "~/components/ui2/ComboBox"
 import { ListBox, ListBoxItem } from "~/components/ui2/ListBox"
+import { Meter } from "~/components/ui2/Meter"
+import { Modal } from "~/components/ui2/Modal"
+import { NumberField } from "~/components/ui2/NumberField"
 import { Radio, RadioGroup } from "~/components/ui2/RadioGroup"
 import { RangeCalendar } from "~/components/ui2/RangeCalendar"
 import { SearchField } from "~/components/ui2/SearchField"
+import { Slider } from "~/components/ui2/Slider"
+import { TimeField } from "~/components/ui2/TimeField"
+import { createToastHeaders, redirectWithToast } from "~/utils/toast.server"
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const orgs = await db.query.organizations.findMany()
@@ -15,7 +21,20 @@ export async function loader({ request }: LoaderFunctionArgs) {
   const sessions = await db.query.sessions.findMany()
   const connections = await db.query.connections.findMany()
 
-  return true
+  // return true
+  return json(
+    {
+      orgs,
+      users,
+      sessions,
+      connections,
+    },
+    // {
+    //   headers: await createToastHeaders({
+    //     description: "This is a test page.",
+    //   }),
+    // },
+  )
   // return redirectWithToast("/", {
   //   title: "Test",
   //   description: "This is a test page.",
@@ -24,7 +43,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function Test() {
   return (
-    <div className="mx-auto max-w-xl">
+    <div className="">
       {/* <SwitchGroup>
         <SwitchField>Test</SwitchField>
         <Switch color="amber" />
@@ -56,8 +75,17 @@ export default function Test() {
         <ListBoxItem>Test</ListBoxItem>
       </ListBox>
 
-      <Button>Test</Button> */}
+      <Button>Test</Button>
       <p className="text-red-500">Test</p>
+      <Modal />
+
+      <NumberField /> */}
+      <Slider />
+      <div>
+        <Meter label="Storage Label" value={99} className="max-w-7xl" />
+      </div>
+
+      <TimeField />
     </div>
   )
 }
