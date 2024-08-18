8418397ef54029e9fe8e4480dfe4f36857d08b82
Alex <alex@gogl.io>
2024-08-18 16:53:26 +0200

    change test

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,26 +1,11 @@
 import { json, type LoaderFunctionArgs } from "@remix-run/node"
 import { db } from "db.server"
-import { requireUserId } from "~/utils/auth.server"
-import {
-  requireUserWithPermission,
-  requireUserWithRole,
-} from "~/utils/multitenancy/permissions.server"
-import { createUserWithOrganization } from "~/utils/multitenancy/user.server"
-import { redirectWithToast } from "~/utils/toast.server"
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const orgs = await db.query.organizations.findMany()
   const users = await db.query.users.findMany()
   const sessions = await db.query.sessions.findMany()
   const connections = await db.query.connections.findMany()
-
-  // await createUserWithOrganization({
-  //   organizationName: "Gogl",
-  //   user: {
-  //     email: "alex@gogl.io",
-  //     name: "Alex",
-  //   },
-  // })
 
   return json({
     orgs,
